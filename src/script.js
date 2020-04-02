c990ea2d03e00fcc5ee5ac6885b917db242b9404
dimkot2019 <dimkot@inbox.ru>
2020-04-03 00:30:02 +0300

    Task Manager Затенение модального окна и его закрытие 2.04

--- src/script.js
+++ src/script.js
@@ -1,18 +1,23 @@
+let shading = document.getElementById('myShading');
 let modal = document.getElementById('myModal');
-let btn = document.getElementById("myBtn");
+let button = document.getElementById("myButton");
 let span = document.getElementsByClassName("Close")[0];
 let savetask = document.getElementsByClassName("SaveTask")[0];
-btn.onclick = () => {
+button.onclick = () => {
     modal.style.display = "block";
+    shading.style.display = "block";
   }
 span.onclick = () => {
     modal.style.display = "none";
+    shading.style.display = "none";
   }
-  savetask.onclick = () => {
+savetask.onclick = () => {
     modal.style.display = "none";
+    shading.style.display = "none";
   }
-  window.onclick = (event) => {
-    if (event.target === modal ) {
+  
+shading.onclick = () => {
       modal.style.display = "none";
-    }
+      shading.style.display = "none";
+    
   }
