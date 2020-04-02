544a6488e32cb7668db8b91d129ce6114962a7cd
dimkot2019 <dimkot@inbox.ru>
2020-04-02 16:51:30 +0300

    Task Manager Открытие и закрытие модального окна 2.04

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
 let modal = document.getElementById('myModal');
 let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("Close")[0];
-btn.onclick = function() {
+let savetask = document.getElementsByClassName("SaveTask")[0];
+btn.onclick = () => {
     modal.style.display = "block";
   }
-span.onclick = function() {
+span.onclick = () => {
     modal.style.display = "none";
   }
-window.onclick = function(event) {
-    if (event.target == modal) {
+  savetask.onclick = () => {
+    modal.style.display = "none";
+  }
+  window.onclick = (event) => {
+    if (event.target === modal ) {
       modal.style.display = "none";
     }
   }
