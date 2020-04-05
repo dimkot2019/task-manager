edca2593a2d5258f162d719d81136452b68ede09
dimkot2019 <dimkot@inbox.ru>
2020-04-05 10:38:38 +0300

    Task Manager Работа над замечаниями 5.04

--- src/script.js
+++ src/script.js
@@ -3,28 +3,24 @@ let modal__window = document.getElementById('myModal__window');
 let button = document.getElementById('myButton');
 let span = document.getElementById('closeModal__window');
 let savetask = document.getElementById('saveTaskButton');
-button.onclick() = showModal();
+/*button.onclick = showModal();
  showModal = () => {
     modal__window.style.display = "block";
     modal.style.display = "block";
-  };
-/*const closeModa = () => {
-    modal__window.style.display = "none";
-    modal.style.display = "none";
   };*/
-/*button.onclick = () => {
+button.onclick = () => {
     modal__window.style.display = "block";
     modal.style.display = "block";
-  };*/
+};
 span.onclick = () => {
     modal__window.style.display = "none";
     modal.style.display = "none";
-  };
+};
 savetask.onclick = () => {
     modal__window.style.display = "none";
     modal.style.display = "none";
-  };
+};
 modal.onclick = () => {
       modal__window.style.display = "none";
       modal.style.display = "none"; 
-  };
+};
